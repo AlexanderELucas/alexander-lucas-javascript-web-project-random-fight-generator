b267a54c4c9066f5ca3f317810d6c393dd531971
Alexander-Lucas <Alexander.Lucas.Cohort225@gmail.com>
2022-09-06 17:41:05 -0700

    app.js outlining

---
+++ app.js
@@ -0,0 +1,35 @@
+//Add new fighter
+    //take fighter input
+    //add to fighter array
+    //clear input fields
+
+
+//Remove fighter
+    //remove fighter from fighter array
+
+//Random Weapon Selection function
+    //Blue/Red/Shield/Ranged/Pole/FightersChoice
+    //random floor selection from 0 to 6 using if statement to filter between options
+    //return fighters selected weapon
+
+//Random Fighter Selection
+    //uses map of fighterArray.length
+    //random floor selection from 0 to fighterArray.length to filter out first fighter
+    //remove first fighter from fighterArray map
+    //random floor selection from 0 to new map fighterArray.length to filter out second fighter
+    //remove second fighter from fighterArray map
+    //return fighters names
+
+
+
+
+
+
+//>>>WANTS<<<
+
+//Save Fighters to local
+
+//Choose which input fighters to randomly select from
+//Choose team size
+
+//Random team size selection
