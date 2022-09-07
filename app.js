cc5d90ac5f1c1a2ff0d56ce2879c0b31aa75dcb8
Alexander-Lucas <Alexander.Lucas.Cohort225@gmail.com>
2022-09-06 23:07:40 -0700

    commit after Martyns notes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
 //Random Weapon Selection function
     //Blue/Red/Shield/Ranged/Pole/FightersChoice
-    //random floor selection from 0 to 6 using if statement to filter between options
+    //random selection from 0 to 6 using if statement to filter between options
     //return fighters selected weapon
 
 //Random Fighter Selection
@@ -21,15 +21,26 @@
     //return fighters names
 
 
-
+//Notes: Object with keys  
+//Call the Object.keys() method to get an array of the object's keys
+//Use the Math.floor() and Math.random() functions to get a random index of the array
+//Use the random index to access one of the object's properties.
 
 
 
 //>>>WANTS<<<
 
 //Save Fighters to local
+    //stringify JSON/fighterArray
+    //save fighter array to local storage API
+    //requires a setup with parsing the JSON
 
 //Choose which input fighters to randomly select from
+    //push selected fighter from fighter array to selectedFighterArray
 //Choose team size
+    //
 
 //Random team size selection
+
+
+//tournament
